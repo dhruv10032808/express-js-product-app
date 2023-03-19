ed28d522b512c89dacb947447eab77ceecfa5061
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-03-19 21:38:23 +0530

    connecting sql database to node

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,10 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null,title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save().then(()=>{
+    res.redirect('/');
+  })
+  .catch(err=>console.log(err))
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -24,7 +26,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId=req.params.productId;
-  Product.findById(prodId,product=>{
+  Product.findById(prodId).then(([product])=>{
     if(!product){
       return res.redirect('/');
     }
@@ -34,7 +36,8 @@ exports.getEditProduct = (req, res, next) => {
     editing:editMode,
     product:product
     });
-  }) 
+  })
+  .catch(err=>console.log(err));
 };
 
 exports.postEditProduct=(req,res,next)=>{
@@ -49,17 +52,20 @@ exports.postEditProduct=(req,res,next)=>{
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows,fieldData])=>{
     res.render('admin/products', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  })
+  .catch(err=>console.log(err))
 };
 
 exports.postDeleteProduct=(req,res,next)=>{
   const prodId=req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteById(prodId).then(()=>{
+    res.redirect('/admin/products');
+  })
+  .catch(err=>console.log(err));
 }
