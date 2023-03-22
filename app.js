7bdea5601dcd72eef736b39ad63643a27145fcd6
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-03-22 16:08:17 +0530

    associations

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const path = require('path');
 const sequelize=require('./util/database')
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const User=require('./models/user')
+const Product=require('./models/product')
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -16,11 +17,35 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+    User.findByPk(1).then(user=>{
+        req.user=user;
+        next();
+    }).catch(err=>console.log(err))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+Product.belongsTo(User);
+User.hasMany(Product);
 sequelize.sync()
-.then(result=>console.log(result))
+.then(result=>{
+    return User.findByPk(1)
+    //console.log(result)
+})
+.then(user=>{
+    if(!user){
+        return User.create({
+            name:'Dhruv',
+            email:'dhruv@gmail.com'
+        })
+    }
+    return Promise.resolve(user)
+})
+.then(user=>{
+    //console.log(user);
+    app.listen(3000)
+})
 .catch(err=>console.log(err))
-app.listen(3000);
